7a774b9c3c7236752bffef161bedffa730268651
Haven <haven.eldebaque@gmail.com>
2023-05-15 01:25:14 +1000

    Add project files.

---
+++ WeekFourReverseMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// Week 4 - Reverse method 
+
+class Program
+{
+    static void Main()
+    {
+        string[] randomeName = {"Rami", "Sami", "Ali", "Salama" };
+        int i;
+
+        Array.Reverse(randomeName);
+        for (i = 0; i < randomeName.Length; i++)
+        {
+            Console.WriteLine(randomeName[i]);
+        }
+    }
+}
